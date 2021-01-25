dcfa0bd8adb1db96f85963e63dce70c47c8ed2d6
Vinicius <viniciusp.olivera@gmail.com>
2021-01-25 19:19:29 -0300

    Gonna refactor everything later

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ cp -r build myriad
 sleep 0.3
 
 cp build/index.html index.html.new
-sed 's/\/static/build\/static/g' index.html.new > index.html.patched
+sed 's/\/static/myriad\/static/g' index.html.new > index.html.patched
 rm index.html.new index.html
 mv index.html.patched index.html
